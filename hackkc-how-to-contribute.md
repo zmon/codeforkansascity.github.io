b9d20b3dc430e5e2b4d80346a0e91151269df3c1
Paul D Barham <paulb@savagesoft.com>
2016-06-08 12:35:36 -0500

    Final hackkc 2016

---
+++ hackkc-how-to-contribute.md
@@ -0,0 +1,35 @@
+---
+layout: blank-wide
+---
+
+# Not sure what to contribute? BASEDEF.
+
+## Blog
+
+When you stumble upon a project written by someone else (your coworker, say) you tend to see a clear high-level overview and this makes you the perfect person to write an introductory blog post explaining the project.
+
+## Apply
+
+The best thing for a new tool is to be put to use.  Apply a project to one of your own.
+
+## Suggest
+
+Follow a projects documentation and see if you can achieve a small victory in less than 5 minutes. Tell the maintainer how it went & what was confusing.
+
+## Extend
+
+Add a new feature, either of your own invention or inspired by a feature request on github.  
+
+## Document
+
+Write a separate set of documents or improve error messaging. Skews toward technical writing.
+
+## Evangelize
+
+Email it, tweet it, submit it to other publications. Just make sure to check with the maintainer, they may prefer to fix problems before recruiting users.
+
+## Fix
+
+Fix specific bugs or improve the general process of project development. Includes adding a test suite, enabling continuous integration, or using static code analyzers.  
+
+Read more about BASEDEF at begriffs.com
---
+++ js/hackkc-project-list.js
@@ -0,0 +1,70 @@
+/* From http://www.nczonline.net/blog/2010/05/25/cross-domain-ajax-with-cross-origin-resource-sharing/ */
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+}
+
+
+// Get Google Spreadsheet data
+
+// https://docs.google.com/spreadsheets/d/1IFbDEk5cRKP3WuQX7gMl6XDxLYuVZ4eeq0XRluxqmEQ/edit#gid=669009687
+//                                        \________________________  _______________/
+//                                                                 \/
+//                                                                key passed to tabletop
+//                                                                to identify the spread sheet
+//  NOTE:  You have to publish the spread sheet from
+//         the File menu of the spread sheet.
+
+// Please note that the column names in this spread sheet were defined by the Google Form that inserts data into it.
+
+
+Tabletop.init({                                             // Requires js/tabletop.js
+    key: '1Dvb4LRZuFkghcQl50R_fSCjkFPoJSLVe8NXiyJyIy40',
+    callback: function (data, tabletop) {
+
+        var converter = new showdown.Converter();
+        for (var i in data) {
+
+            if ( data[i]['Display'] != 'Y' ) continue;
+            var row = '';
+
+            row += '<div class="col-md-12">';
+            row += '<h2 style="font-size: 42pt">' + data[i]['Title'] + '</h2>';
+            row += '                    <h3 style="font-size: 34pt">' + converter.makeHtml(data[i]['Short Desciption']) + '</h3>';
+
+            row += '                    <h3 style="font-szie: 28pt">Lead: ' + data[i]['Lead'] + '</h3>';
+            row += '</div>';
+            row += '<div class="col-md-6">';
+            row += '                    <h3 style="font-szie: 28pt">Team: ' + converter.makeHtml(data[i]['Team']) + '</h3>';
+            row += '</div>';
+            row += '<div class="col-md-6">';
+            row += '                    <h3 style="font-szie: 28pt">Needs: ' + converter.makeHtml(data[i]['Needs']) + '</h3>';
+            row += '</div>';
+            row += '<div class="col-md-12">';
+            row += '                    <h3 style="font-szie: 28pt">Slack: ' + data[i]['Slack'] + '</h3>';
+            row += '                    <h3 style="font-szie: 28pt">Code: ' + data[i]['GitHub'] + '</h3>';
+            row += '                    <h3 style="font-szie: 28pt">Site: ' + data[i]['Site'] + '</h3>';
+
+            row += '<br><br><br><br>';
+            row += '</div>';
+
+
+            $('#projects').append(row);
+
+
+        }
+    },
+    orderby: 'Type of Entity',
+    simpleSheet: true
+});
+
+
